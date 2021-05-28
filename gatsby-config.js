8f30114f49b35df26fcf3ebf569d76d2a23de336
Martin Nepita <35706069+mnepita@users.noreply.github.com>
2021-05-28 00:30:18 -0700

    added siteMeta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  siteMetadata: {
+    description: "Personal page of Martin Nepita",
+    locale: "en",
+    title: "Martin Nepita",
+    formspreeEndpoint: "https://formspree.io/f/xqkwqpjn",
+  },
     plugins: [
       {
         resolve: "@wkocjan/gatsby-theme-intro",
